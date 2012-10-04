2720111f0e23e078ba333c8846125b9c28de596f
jacknolan <jack@jacknolan.com>
2012-10-04 17:36:26 -0400

    fixed primary keys

--- quiz.sql
+++ quiz.sql
@@ -1,30 +1,30 @@
--- CREATE TABLE user (
--- 	id INT PRIMARY KEY,   
--- 	name VARCHAR(255)
--- );
+ CREATE TABLE user (
+ 	id INTEGER PRIMARY KEY,   
+ 	name VARCHAR(255)
+ );
 
 CREATE TABLE quiz (
-	id INT PRIMARY KEY,   
+	id INTEGER PRIMARY KEY,   
 	name VARCHAR(255),
-	owner_id INT NOT NULL
+	owner_id INTEGER NOT NULL
 );
 
 CREATE TABLE question (
-	id INT PRIMARY KEY,   
+	id INTEGER PRIMARY KEY,   
 	content VARCHAR(255),
-	answer_id INT NOT NULL,
-	quiz_id INT NOT NULL
+	answer_id INTEGER NOT NULL,
+	quiz_id INTeger NOT NULL
 );
 
 CREATE TABLE answer (
-	id INT PRIMARY KEY,
+	id INTEGER PRIMARY KEY,
 	content VARCHAR(255),   
-	question_id INT NOT NULL
+	question_id INTEGER NOT NULL
 );
 
 CREATE TABLE user_response (
-	user_id INT NOT NULL,
-	question_id INT NOT NULL,
-	answer_id INT NOT NULL,
+	user_id INTEGER NOT NULL,
+	question_id INTEGER NOT NULL,
+	answer_id INTeger NOT NULL,
 	correct BOOLEAN NOT NULL
 );
